88e12cbbb335e14673eea159a16139e1b05954fc
Desperado <113103241+desperadoNikita@users.noreply.github.com>
2023-02-07 00:07:23 +0200

    Add files via upload

---
+++ example.js
@@ -0,0 +1,7 @@
+ const goods = "pears";
+ const price = "5";
+ const amount = "8";
+ const value = price * amount;
+console.log('value :>> ', value);
+const message = "Ви обрали ${goods}, ціна за предмет ${value} гривень";
+console.log("message :>> ", message);
